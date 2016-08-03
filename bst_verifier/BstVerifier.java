a1dc79aaeb3324786d5869df98773bfd75b85070
AkshayPall <iosakshay@gmail.com>
2016-08-03 15:59:14 -0400

    working away...

---
+++ bst_verifier/BstVerifier.java
@@ -0,0 +1,33 @@
+/* Hidden stub code will pass a root argument to the function below. Complete the function to solve the challenge. Hint: you may want to write one or more helper functions.  
+
+The Node class is defined as follows:
+    class Node {
+        int data;
+        Node left;
+        Node right;
+     }
+*/
+    boolean checkBST(Node root) {
+        boolean isBST = true;
+        
+        if (root.left != null){
+            if (root.left.data > root.data){
+                isBST = false;
+            } else {
+                isBST = checkBST(root.left);
+            }
+        }
+        if (root.right != null){
+            if (root.right.data < root.data){
+                isBST = false;
+            } else {
+                boolean rBstCheck = checkBST(root.right);
+                if (!rBstCheck || !isBST){
+                    isBST = false;
+                }
+            }
+        }
+        
+        
+        return isBST;
+    }
